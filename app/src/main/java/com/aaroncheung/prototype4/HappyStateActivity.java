56532bf9ec6701dee64c620b8238ad1daea418f1
Aaron Cheung <aaroncheung123@gmail.com>
2018-05-25 13:43:19 -0600

    Super class completed

--- app/src/main/java/com/aaroncheung/prototype4/HappyStateActivity.java
+++ app/src/main/java/com/aaroncheung/prototype4/HappyStateActivity.java
@@ -1,36 +1,24 @@
 package com.aaroncheung.prototype4;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 import com.aaroncheung.prototype4.hardware.RobotFacade;
 import com.aaroncheung.prototype4.hardware.SpeechRecognitionActivity;
 import com.aaroncheung.prototype4.states.HappyState;
 import com.aaroncheung.prototype4.states.RobotState;
 
-import java.util.ArrayList;
 
-public class HappyStateActivity extends SpeechRecognitionActivity implements RecognitionListener {
+public class HappyStateActivity extends SpeechRecognitionActivity {
 
     public final static String TAG = "debug_main4";
     private RobotState robotState;
     private RobotFacade robotFacade;
 
-    private SpeechRecognizer speech = null;
-    private Intent recognizerIntent;
-    private static final int REQUEST_RECORD_PERMISSION = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +38,6 @@ public class HappyStateActivity extends SpeechRecognitionActivity implements Rec
         robotState = RobotState.getInstance();
         robotFacade = RobotFacade.getInstance();
 
-
-        speech = SpeechRecognizer.createSpeechRecognizer(this);
-        Log.i(TAG, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this));
-        speech.setRecognitionListener(this);
-        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
-                "en");
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
-
         begin();
 
     }
@@ -72,147 +49,9 @@ public class HappyStateActivity extends SpeechRecognitionActivity implements Rec
         Log.d(TAG, "HAPPYSTATEACTIVITY2");
     }
 
-    public void happyStateFaceClick(View view){
-//        Intent myIntent = new Intent(this, SadStateActivity.class);
-//        this.startActivity(myIntent);
-
-        ActivityCompat.requestPermissions
-                (HappyStateActivity.this,
-                        new String[]{Manifest.permission.RECORD_AUDIO},
-                        REQUEST_RECORD_PERMISSION);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_RECORD_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    speech.startListening(recognizerIntent);
-                } else {
-                    Toast.makeText(HappyStateActivity.this, "Permission Denied!", Toast
-                            .LENGTH_SHORT).show();
-                }
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (speech != null) {
-            speech.destroy();
-            Log.i(TAG, "destroy");
-        }
+    public void happyStateFaceClick(View view) {
+        startListening();
     }
 
 
-    @Override
-    public void onBeginningOfSpeech() {
-        Log.i(TAG, "onBeginningOfSpeech");
-        //progressBar.setIndeterminate(false);
-        //progressBar.setMax(10);
-    }
-
-    @Override
-    public void onBufferReceived(byte[] buffer) {
-        Log.i(TAG, "onBufferReceived: " + buffer);
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        Log.i(TAG, "onEndOfSpeech");
-        //progressBar.setIndeterminate(true);
-        //toggleButton.setChecked(false);
-    }
-
-    @Override
-    public void onError(int errorCode) {
-        String errorMessage = getErrorText(errorCode);
-        Log.d(TAG, "FAILED " + errorMessage);
-        //returnedText.setText(errorMessage);
-        //toggleButton.setChecked(false);
-    }
-
-    @Override
-    public void onEvent(int arg0, Bundle arg1) {
-        Log.i(TAG, "onEvent");
-    }
-
-    @Override
-    public void onPartialResults(Bundle arg0) {
-        Log.i(TAG, "onPartialResults");
-    }
-
-    @Override
-    public void onReadyForSpeech(Bundle arg0) {
-        Log.i(TAG, "onReadyForSpeech");
-    }
-
-    @Override
-    public void onResults(Bundle results) {
-        Log.i(TAG, "onResults");
-        ArrayList<String> matches = results
-                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        String text = "";
-        for (String result : matches)
-            text += result + "\n";
-
-        Log.i(TAG, matches.get(0));
-
-        //returnedText.setText(text);
-    }
-
-    @Override
-    public void onRmsChanged(float rmsdB) {
-        Log.i(TAG, "onRmsChanged: " + rmsdB);
-        //progressBar.setProgress((int) rmsdB);
-    }
-
-    public static String getErrorText(int errorCode) {
-        String message;
-        switch (errorCode) {
-            case SpeechRecognizer.ERROR_AUDIO:
-                message = "Audio recording error";
-                break;
-            case SpeechRecognizer.ERROR_CLIENT:
-                message = "Client side error";
-                break;
-            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                message = "Insufficient permissions";
-                break;
-            case SpeechRecognizer.ERROR_NETWORK:
-                message = "Network error";
-                break;
-            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-                message = "Network timeout";
-                break;
-            case SpeechRecognizer.ERROR_NO_MATCH:
-                message = "No match";
-                break;
-            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                message = "RecognitionService busy";
-                break;
-            case SpeechRecognizer.ERROR_SERVER:
-                message = "error from server";
-                break;
-            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-                message = "No speech input";
-                break;
-            default:
-                message = "Didn't understand, please try again.";
-                break;
-        }
-        return message;
-    }
 }
